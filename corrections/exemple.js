cbdbdc968a7d97f6b7eab6c2fb68c44fa5579a49
moguy93 <guymorgan@live.fr>
2023-09-25 13:33:38 +0200

    add pdf

---
+++ corrections/exemple.js
@@ -0,0 +1,23 @@
+let person = {
+    name: "morgan",
+    age: 9,
+    foo: {
+        lastname: "guy",
+        skills: [
+            "peindre",
+            "coder"
+        ]
+    }
+}
+
+// formatage de string en js
+let var1 = 34
+// if (perso.foo.skills)
+//     print(`Bonjour ${var1}`)
+// else if () {
+
+// }
+// else {
+
+// }
+
