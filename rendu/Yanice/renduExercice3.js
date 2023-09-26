cd4284894fd37237564f031d8b5298251158447a
Yanice <yanice.trech@non.se.com>
2023-09-26 12:02:57 +0200

    - Updated because useless backslash

--- rendu/Yanice/renduExercice3.js
+++ rendu/Yanice/renduExercice3.js
@@ -1,5 +1,5 @@
 const apiResult = "{ \"id\": 1, \"result\": [ { \"status\": { \"code\": 0, \"message\": \"OK\" }, \"url\": \"\\/sys\\/login\\/user\" } ]}";
 const apiResultWithoutBacklash = apiResult.replace(/\\+/g, "");
-const apiResultHarmonized = apiResultWithoutBacklash.replace(/"([^"]*[^\\])"/g, "'$1'");
+const apiResultHarmonized = apiResultWithoutBacklash.replace(/"([^"]*)"/g, "'$1'");
 
 console.log(apiResultHarmonized);
